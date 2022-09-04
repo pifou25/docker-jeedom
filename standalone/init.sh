1c64d723cc1e711c239101c9e52d213c6aa5f789
pifou25 <nicolas_gras@hotmail.com>
2022-09-04 22:40:09 +0200

    aaa

--- standalone/init.sh
+++ standalone/init.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# ___________________________
+#   constants and functions
+# ___________________________
 VERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
 ROUGE="\\033[1;31m"
@@ -33,8 +37,16 @@ mysql_sql() {
   fi
 }
 
+# ___________________________
+#   starting...
+# ___________________________
+
 log_info "starting jeedom ${JEEDOM_BRANCH}"
 cd /var/www/html
+
+# ___________________________
+#   first start: download sources
+# ___________________________
 
 if [ ! -f "/var/www/html/index.php" ]; then
     log_debug "git clone jeedom ${JEEDOM_BRANCH}"
@@ -47,6 +59,10 @@ if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
     exit 1
   fi
 
+  # ___________________________
+  #   configure database
+  # ___________________________
+
   log_info "first run of jeedom container : configuration"
   cp /var/www/html/core/config/common.config.sample.php /var/www/html/core/config/common.config.php
   sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" /var/www/html/core/config/common.config.php
@@ -54,11 +70,20 @@ if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
   sed -i "s/#USERNAME#/${MYSQL_JEEDOM_USER}/g" /var/www/html/core/config/common.config.php
   sed -i "s/#PORT#/3306/g" /var/www/html/core/config/common.config.php
   sed -i "s/#HOST#/${MYSQL_HOST}/g" /var/www/html/core/config/common.config.php
-  chmod 775 -R /var/www/html
+  
+  chmod 770 -R /var/www/html
   chown -R www-data:www-data /var/www/html
   mkdir -p /tmp/jeedom
-  chmod 777 -R /tmp/jeedom
+  chmod 770 -R /tmp/jeedom
   chown www-data:www-data -R /tmp/jeedom
+
+  log_info " ___ Start mysql ___"
+  # /usr/sbin/mysqld
+  supervisorctl start mysql
+  if [ $? -ne 0 ]; then
+    log_error "Ne peut lancer mysql - Annulation"
+    exit 1
+  fi
 
   log_info "Création de la database SQL ${MYSQL_JEEDOM_DATABASE}..."
   mysql_sql "DROP USER IF EXISTS '${MYSQL_JEEDOM_USER}'@'%';"
@@ -104,32 +129,11 @@ if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
   log_info " ___ successfull new installation ! ___"
 fi
 
-# start all services 
-
-systemctl daemon-reload
-log_info " ___ Start mysql ___"
-service mysql start
-service mysql status
-if [ $? -ne 0 ]; then
-  log_error "Ne peut lancer mysql - Annulation"
-  exit 1
-fi
-
-log_info " ___ Start fail2ban ___"
-service fail2ban start 
-if [ $? -ne 0 ]; then
-  log_error "Ne peut lancer fail2ban - Annulation"
-  exit 1
-fi
-
 sysctl vm.swappiness=10
 
 a2dismod status
 a2enmod headers
 a2enmod remoteip
-  
-# start at daemon
-atd
 
-# start apache
-exec apache2-foreground
+# start supervisord
+exec /usr/bin/supervisord
