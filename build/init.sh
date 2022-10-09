95ece7d725a72d171111db45ac8078947908b24b
pifou25 <nicolas_gras@hotmail.com>
2022-10-09 15:40:03 +0200

    transform win eol into linux eol

--- build/init.sh
+++ build/init.sh
@@ -13,49 +13,73 @@ log_error() {
   echo "${ROUGE}$@${NORMAL}"
 }
 
+log_warn() {
+  echo "${JAUNE}$@${NORMAL}"
+}
+
+log_info() {
+  echo "${VERT}$@${NORMAL}"
+}
+
+log_debug() {
+  echo "${BLEU}$@${NORMAL}"
+}
+
 mysql_sql() {
-  echo "$@" | mysql -uroot
+  echo "$@" | mysql -uroot "-p${MYSQL_ROOT_PASSWORD}"
   if [ $? -ne 0 ]; then
     log_error "Ne peut exécuter $@ dans MySQL - Annulation"
     exit 1
   fi
 }
 
-log_error "starting jeedom ${JEEDOM_BRANCH}"
+# ___________________________
+#   starting...
+# ___________________________
+
+log_info "starting jeedom ${JEEDOM_BRANCH}"
 cd /var/www/html
 
+# ___________________________
+#   first start: download sources
+# ___________________________
+
 if [ ! -f "/var/www/html/index.php" ]; then
-    echo "${BLEU}git clone jeedom ${JEEDOM_BRANCH} ${NORMAL}"
+    log_debug "git clone jeedom ${JEEDOM_BRANCH}"
     git clone https://github.com/jeedom/core.git -b ${JEEDOM_BRANCH} .
 fi
 
 if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
-    if [ ! -f "/var/www/html/core/config/common.config.sample.php" ]; then
-      log_error "Can not install jeedom (no config.sample file)"
-      exit 1
-    fi
+  if [ ! -f "/var/www/html/core/config/common.config.sample.php" ]; then
+    log_error "Can not install jeedom (no config.sample file)"
+    exit 1
+  fi
 
-    echo "${JAUNE}first run of jeedom container : configuration${NORMAL}"
+  # ___________________________
+  #   configure database
+  # ___________________________
+
+  log_info "first run of jeedom container : configuration"
     cp /var/www/html/core/config/common.config.sample.php /var/www/html/core/config/common.config.php
     sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" /var/www/html/core/config/common.config.php
     sed -i "s/#DBNAME#/${MYSQL_JEEDOM_DATABASE}/g" /var/www/html/core/config/common.config.php
     sed -i "s/#USERNAME#/${MYSQL_JEEDOM_USER}/g" /var/www/html/core/config/common.config.php
     sed -i "s/#PORT#/3306/g" /var/www/html/core/config/common.config.php
     sed -i "s/#HOST#/${MYSQL_HOST}/g" /var/www/html/core/config/common.config.php
-    chmod 775 -R /var/www/html
+    chmod 770 -R /var/www/html
     chown -R www-data:www-data /var/www/html
     mkdir -p /tmp/jeedom
-    chmod 777 -R /tmp/jeedom
+    chmod 770 -R /tmp/jeedom
     chown www-data:www-data -R /tmp/jeedom
 
-    echo "${JAUNE}Création de la database SQL ${MYSQL_JEEDOM_DATABASE}...${NORMAL}"
-    mysql_sql "DROP USER IF EXISTS '${MYSQL_JEEDOM_USER}'@'localhost';"
-    mysql_sql "CREATE USER '${MYSQL_JEEDOM_USER}'@'localhost' IDENTIFIED BY '${MYSQL_JEEDOM_PASSWD}';"
+    log_info "Création de la database SQL ${MYSQL_JEEDOM_DATABASE}..."
+    mysql_sql "DROP USER IF EXISTS '${MYSQL_JEEDOM_USER}'@'%';"
+    mysql_sql "CREATE USER '${MYSQL_JEEDOM_USER}'@'%' IDENTIFIED BY '${MYSQL_JEEDOM_PASSWD}';"
     mysql_sql "DROP DATABASE IF EXISTS ${MYSQL_JEEDOM_DATABASE};"
     mysql_sql "CREATE DATABASE ${MYSQL_JEEDOM_DATABASE};"
     mysql_sql "GRANT ALL PRIVILEGES ON ${MYSQL_JEEDOM_DATABASE}.* TO '${MYSQL_JEEDOM_USER}'@'localhost';"
 
-    echo "${VERT}jeedom clean install${NORMAL}"
+    log_info "jeedom clean install"
     php /var/www/html/install/install.php mode=force
     if [ $? -ne 0 ]; then
       log_error "Can not install jeedom (error in install.php, see log)"
@@ -63,11 +87,11 @@ if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
     fi
 
     if [ -d "/tmp/backup" ] && [ "$(ls -A /tmp/backup)" ]; then
-       echo "${VERT}found a backup, try to restore...${NORMAL}"
+       log_info "found a backup, try to restore..."
        cp /tmp/backup/* /var/www/html/backup
        php /var/www/html/install/restore.php
     fi
-    echo "${VERT}successfull new installation !${NORMAL}"
+    log_info " ___ successfull new installation ! ___"
 fi
 
 # start at daemon
