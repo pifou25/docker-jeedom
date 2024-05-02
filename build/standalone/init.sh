2849a0cda7eed0e78f3bd5477466d4d75255614a
Pifou25 <nicolas_gras@hotmail.com>
2024-05-02 11:14:21 +0200

    feat: master_phpx.x branch name from any repo
    fix: xdebug step condition
    fix: init sequence and mysql permission
    feat: master_phpx.x branch name from pifou25/jeedom-core

--- build/standalone/init.sh
+++ build/standalone/init.sh
@@ -74,8 +74,7 @@ if [ ! -f "${WEBSERVER_HOME}/core/config/common.config.php" ]; then
   # changes for mysql socket instead of tcp for local use
   # sed -i "s/'host'/'unix_socket'/g" ${WEBSERVER_HOME}/core/config/common.config.php
   # sed -i "s/#HOST#/\/run\/mysqld\/mysqld.sock/g" ${WEBSERVER_HOME}/core/config/common.config.php
-
-
+  
   chmod 770 -R ${WEBSERVER_HOME}
   chown -R www-data:www-data ${WEBSERVER_HOME}
   mkdir -p /tmp/jeedom
@@ -98,7 +97,6 @@ if [ ! -f "${WEBSERVER_HOME}/core/config/common.config.php" ]; then
   mysql_sql "DROP USER IF EXISTS '${MYSQL_JEEDOM_USER}'@'localhost';"
   mysql_sql "CREATE USER '${MYSQL_JEEDOM_USER}'@'localhost' IDENTIFIED BY '${MYSQL_JEEDOM_PASSWD}';"
   mysql_sql "GRANT ALL PRIVILEGES ON ${MYSQL_JEEDOM_DATABASE}.* TO '${MYSQL_JEEDOM_USER}'@'localhost';"
-
 
   log_info "jeedom clean install"
   php ${WEBSERVER_HOME}/install/install.php mode=force
